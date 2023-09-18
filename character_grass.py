32fb1b30bafa9923038bad3a5f8dfba5db03e95e
JeonDae <james911256@gmail.com>
2023-09-18 17:16:09 +0900

    6시 출발, y축 +200 수정

--- character_grass.py
+++ character_grass.py
@@ -7,7 +7,7 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 x, y = 400, 0
-angle = 0
+angle =270
 while (1):
     while (x < 800):
         clear_canvas_now()
@@ -42,9 +42,10 @@ while (1):
         character.draw_now(x,y)
         x = x + 5
         delay(0.01)
-    while(angle < 360):
+    while(angle < 360 + 270):
             clear_canvas_now()
             grass.draw_now(400,30)
+            character.draw_now(x,y+200)
             x = x + 200 * math.cos(math.radians(angle))
             y = y + 200 * math.sin(math.radians(angle))
             delay(0.01)
