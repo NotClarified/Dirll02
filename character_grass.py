757653cae79e46eeabc4ec86649aee92d5ad9c0f
truebird2201 <104574307+NotClarified@users.noreply.github.com>
2023-09-18 15:17:35 +0900

    Drill02 , 2020150038, 전대권
    원형 x 사각형으로 진행됨

--- character_grass.py
+++ character_grass.py
@@ -7,72 +7,74 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 x, y = 400, 0
+xAngle, yAngle = 0,0
 angle = 0
 while (1):
     while (x < 800):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        x = x + 2
+        x = x + 5
         delay(0.01)
     while (y < 600):
             clear_canvas_now()
             grass.draw_now(400,30)
             character.draw_now(x,y)
-            y = y + 2
+            y = y + 5
             delay(0.01)
     y = 600
     while (x >0):
             clear_canvas_now()
             grass.draw_now(400,30)
             character.draw_now(x,y)
-            x = x - 2
+            x = x - 5
             delay(0.01)
     x = 0
     while (y >0):
             clear_canvas_now()
             grass.draw_now(400,30)
             character.draw_now(x,y)
-            y = y - 2
+            y = y - 5
             delay(0.01)
     y = 0
     while (x < 400):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        x = x + 2
+        x = x + 5
         delay(0.01)
     while(angle < 90):
             clear_canvas_now()
             grass.draw_now(400,30)
-            character.draw_now(x*30, y*30)
-            x = x - math.sin(2)
-            y = y - math.cos(2)
+            character.draw_now(x + xAngle*5, y + yAngle*5)
+            xAngle = xAngle - math.sin(2)
+            yAngle = yAngle - math.cos(2)
             delay(0.01)
             angle = angle + 2
     while(angle < 180):
             clear_canvas_now()
             grass.draw_now(400,30)
-            character.draw_now(x*30, y*30)
-            x = x - math.sin(2)
-            y = y + math.cos(2)
+            character.draw_now(x + xAngle*5, y + yAngle*5)
+            xAngle = xAngle - math.sin(2)
+            yAngle = yAngle + math.cos(2)
             delay(0.01)
             angle = angle + 2
     while(angle < 270):
             clear_canvas_now()
             grass.draw_now(400,30)
-            character.draw_now(x*30, y*30)
-            x = x + math.sin(2)
-            y = y + math.cos(2)
+            character.draw_now(x + xAngle*5, y + yAngle*5)
+            xAngle = xAngle + math.sin(2)
+            yAngle = yAngle + math.cos(2)
             delay(0.01)
             angle = angle + 2
     while(angle < 360):
             clear_canvas_now()
             grass.draw_now(400,30)
-            character.draw_now(x*30, y*30)
-            x = x + math.sin(2)
-            y = y - math.cos(2)
+            character.draw_now(x + xAngle*5, y + yAngle*5)
+            xAngle = xAngle + math.sin(2)
+            yAngle = yAngle - math.cos(2)
             delay(0.01)
-            angle = angle + 2     
+            angle = angle + 2
+    angle = 0
 close_canvas()
     
